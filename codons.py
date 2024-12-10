60c65e8f52bab6bfc7b9c5fb6b1c725f2133d736
Talelkarif1 <Talelkarif1186@gmail.com>
2024-12-10 22:03:35 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -5,7 +5,7 @@ def create_codon_dict(file_path):
     d = {}
     for row in rows:
         cells = row.strip().split('\t')
-        condon = cells[0]
+        codon = cells[0]
         amino_acid = cells[2] 
         d[codon] = amino_acid
      return d   
